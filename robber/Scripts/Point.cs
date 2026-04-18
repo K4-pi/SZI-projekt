03dccf03c2f0eae0f0f8669101cc8026e72daf91
K4-pi <Kap1_priv@proton.me>
2026-04-18 14:01:42 +0200

    build fixes

--- robber/Scripts/Point.cs
+++ robber/Scripts/Point.cs
@@ -5,13 +5,11 @@ using System.Collections.Generic;
 public partial class Point : Node2D
 {
 	public bool visited {get; set;}
-	// public Point[] neighbors {get; set;}
-	public List<Point> neighbors {get; set;}
+	public List<Point> neighbors = new List<Point>();
 
 	public void Setup(Vector2 globalPos)
     {
         GlobalPosition = globalPos;
 		visited = false;
-		neighbors = new List<Point>();
     }
 }
